72258d4a86cd104ace4b968cfbd8351c90c0a8fa
xithrius <xithrius@gmail.com>
2020-12-23 17:55:49 -0800

    Added bot subclass.

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,25 @@
-from .bot import start_bot
+import time
+from os import environ
 
-start_bot()
+from discord import Intents
+from loguru import logger
+
+from bot.bot import HolidayBot
+
+environ["TZ"] = "America/Los_Angeles"
+time.tzset()
+
+intents = Intents.default()
+intents.members = True
+
+bot = HolidayBot(
+    command_prefix="hack ",
+    case_insensitive=False,
+    intents=intents,
+)
+
+for extension in bot.get_extensions():
+    bot.load_extension(extension)
+    logger.info(f'Loaded extension "{extension}"')
+
+bot.run(environ.get("BOT_TOKEN"), bot=True, reconnect=True)
--- bot/bot.py
+++ bot/bot.py
@@ -1,23 +1,45 @@
-from os import getenv
+import asyncio
+from os import listdir
+from pathlib import Path
+from typing import List
 
-from discord import Intents
-from discord.ext import commands
+import aiohttp
+from discord import Activity, ActivityType
+from discord.ext.commands import Bot
+from loguru import logger
 
-intents = Intents.default()
-intents.members = True
-
-prefixes = ('hack ', 'Hack ')
-extensions = ['admin', 'activity', 'data', 'event']
-
-
-def start_bot():
-    bot = commands.Bot(command_prefix=prefixes, intents=intents)
-
-    for name in extensions:
-        bot.load_extension(f'bot.extensions.{name}')
-
-    bot.run(getenv('BOT_TOKEN'))
+PRESENCE_URL = "https://holiday.foothillcs.club"
 
 
-if __name__ == '__main__':
-    start_bot()
+class HolidayBot(Bot):
+    """Setting up all the important things."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.loop = asyncio.get_event_loop()
+        self.http_session = aiohttp.ClientSession()
+
+    async def on_ready(self) -> None:
+        """Updates the console when the bot is ready to use."""
+        await self.change_presence(activity=Activity(type=ActivityType.playing, name=PRESENCE_URL))
+
+        logger.info("Awaiting...")
+
+    async def logout(self) -> None:
+        """Subclassing the logout command to ensure connection(s) are closed properly."""
+        await asyncio.wait_for(self.http_session.close(), 30.0, loop=self.loop)
+
+        logger.info("Finished up closing task(s).")
+
+        return await super().logout()
+
+    @staticmethod
+    def get_extensions() -> List[str]:
+        """Gets the extensions from the extension folder."""
+        exts = [
+            f"bot.extensions.{ext[:-3]}"
+            for ext in [x for x in listdir(Path("bot/extensions")) if x.endswith(".py")]
+        ]
+
+        return exts
